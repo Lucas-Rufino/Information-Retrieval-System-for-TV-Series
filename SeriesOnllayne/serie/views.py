892f821fa79aa7847ea52a77bd9c8f6a7271f4a0
rllima <rlo@cin.ufpe.br>
2018-07-03 17:20:50 -0300

    add files

--- SeriesOnllayne/serie/views.py
+++ SeriesOnllayne/serie/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def search_page(request):
+    return render(request, 'series/search.html')
--- SeriesOnllayne/seriesOnllayne/urls.py
+++ SeriesOnllayne/seriesOnllayne/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path(r'', include('serie.urls'))
 ]
 
