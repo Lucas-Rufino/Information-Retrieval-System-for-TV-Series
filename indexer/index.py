6f6e71f32b1c89ddf9bc205a10b79d1ca8dc38d7
Lucas Alves Rufino <lucas@iMac-de-Lucas.local>
2018-07-04 17:10:55 -0300

    Function to return sum e mean of terms implemented

--- indexer/index.py
+++ indexer/index.py
@@ -6,6 +6,7 @@ class Inverted(object):
     def __init__(self, local):
         self._attrs = set(['title', 'genre', 'rate', 'resume', 'cast'])
         self._local = local
+        self._size = {}
         self._db = {}
 
     def load(self, mode='normal', serialize=False):
@@ -19,6 +20,8 @@ class Inverted(object):
             self._db = decode.interval(data, self._local)
         elif mode ==  'bytecode':
             self._db = decode.bytecode(data, self._local)
+        with open('indexer/database/sizes.json') as fl:
+            self._size = json.load(fl)
 
     def save(self, mode='normal', serialize=False):
         data = None
@@ -31,16 +34,23 @@ class Inverted(object):
         elif mode ==  'bytecode':
             data = encode.bytecode(self._db, self._local)
         with open(path, 'wb' if serialize else 'w') as fl:
-            if serialize:
-                pickle.dump(data, fl)
-            else:
-                json.dump(data, fl)
+            pickle.dump(data, fl) if serialize else json.dump(data, fl)
+        with open('indexer/database/sizes.json', 'w') as fl:
+            json.dump(self._size, fl)
 
     def covertAll(self, by):
         all = by.get('all', None)
         if all is not None:
             by = { attr:all for attr in self._attrs }
         return by
+
+    def meanDocs(self):
+        mean = (sum(self._size.values()))/float(len(self._size))
+        return mean
+
+    def sumDocs(self):
+        total = sum(self._size.values())
+        return total
 
 class Basic(Inverted):
     def __init__(self):
@@ -52,6 +62,8 @@ class Basic(Inverted):
         for word in words:
             aux = self._db.setdefault(attr, {})
             aux = aux.setdefault(word, [])
+            self._size.setdefault(id, 0)
+            self._size[id] += 1
             if id not in aux:
                 aux.append(id)
 
@@ -82,6 +94,8 @@ class Frequency(Inverted):
             aux = self._db.setdefault(attr, {})
             aux = aux.setdefault(word, {})
             aux.setdefault(id, 0)
+            self._size.setdefault(id, 0)
+            self._size[id] += 1
             aux[id] += 1
 
     def search(self, by):
@@ -111,6 +125,8 @@ class Positional(Inverted):
             aux = self._db.setdefault(attr, {})
             aux = aux.setdefault(word, {})
             aux = aux.setdefault(id, [])
+            self._size.setdefault(id, 0)
+            self._size[id] += 1
             aux.append(i)
 
     def search(self, by):
